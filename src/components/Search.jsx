ba6a9114e88d426f212687d134f0926078c3b722
aneesh072 <aneeshkarki292@gmail.com>
2022-10-09 17:09:24 +1100

    added user and search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react';
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder='find a user'/>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/3813324/pexels-photo-3813324.jpeg?auto=compress&cs=tinysrgb&w=800" />
+        <div className="userChatInfo">
+          <span>Enis</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
