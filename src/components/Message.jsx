7ccaae5512ae378410fc443a1333cbb78b992a1e
aneesh072 <aneeshkarki292@gmail.com>
2022-10-09 19:17:14 +1100

    finalised designe

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React from 'react';
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/3813324/pexels-photo-3813324.jpeg?auto=compress&cs=tinysrgb&w=800"
+          alt=""
+        />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img
+          src="https://images.pexels.com/photos/3813324/pexels-photo-3813324.jpeg?auto=compress&cs=tinysrgb&w=800"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
