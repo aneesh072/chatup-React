a46a28b4c7927d9475789051eb5ac5f43513bae6
aneesh072 <aneeshkarki292@gmail.com>
2022-10-10 18:55:36 +1100

    use context for the userID

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './style.scss';
+import { AuthContext } from './context/AuthContext';
 
 const App = () => {
-  return <Register />;
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          ></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="register" element={<Register />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,9 +4,11 @@ import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth, storage, db } from '../firebase';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { doc, setDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [err, setErr] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -40,10 +42,13 @@ const Register = () => {
               email,
               photoURL: downloadURL,
             });
+
+            await setDoc(doc(db, 'userChats', res.user.uid), {});
+            navigate('/');
           });
         }
       );
-    } catch (error) {
+    } catch (err) {
       setErr(true);
     }
   };
