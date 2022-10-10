357e754a9846dcf6f21867b4c6df4c53930b6307
aneesh072 <aneeshkarki292@gmail.com>
2022-10-10 16:11:24 +1100

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  /* YOUR COFIGS FROM FIREBASE */
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
