dfaabc34c26de47f6fde44fe6e871e4f6552eea3
aneesh072 <aneeshkarki292@gmail.com>
2022-10-11 17:05:58 +1100

    current user display on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
+import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className="navbar">
       <span className="logo"> Lets Chat</span>
       <div className="user">
-        <img
-          src="https://images.pexels.com/photos/3813324/pexels-photo-3813324.jpeg?auto=compress&cs=tinysrgb&w=800"
-          alt=""
-        />
-        <span>Enis</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>Logout</button>
       </div>
     </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import Add from '../assets/addAvatar.png';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
 
 const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate('/');
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Lets Chat</span>
         <span className="title">Login</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
 
           <button>Sign In</button>
+          {err && <span>Something went wrong</span>}
         </form>
-        <p>You do have an account? Register!</p>
+        <p>
+          You do have an account? <Link to="/register">Register!</Link>
+        </p>
       </div>
     </div>
   );
